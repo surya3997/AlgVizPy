b095ed0e6e5d2654a8297ae23804e8c9ce71fa09
Guru Prasath <guru98@live.com>
2018-10-27 15:32:34 +0530

    Modified implementaion as in the pseudo code

--- Root_finding/Bairstow.py
+++ Root_finding/Bairstow.py
@@ -1,87 +1,76 @@
-from sympy import *
-import math
+def quadroot(r,s):
+	"""
+	Finds the Quadratic Root of r and s.
+	"""
 
-deg = 5
+	import math
 
-coeff = [1, -3.5, 2.75, 2.125, -3.875, 1.25]
-
-r = -1
-s = -1
-
-A = coeff[:]
-
-precision = 4
-
-stopper = 20
-
-def calculate_c(r, s, B):
-    n = len(A)
-    
-    if n > 3:
-        C = []
-
-        C.append(B[0])
-        C.append(B[1] + r * C[0])
-
-        for i in range(2, len(A) - 1):
-            C.append(B[i] + r * C[i - 1] + s * C[i - 2])
-
-        #print(C)
-        return(C)
-
-def calculate_b(r, s):
-    n = len(A)
-
-    if n > 3:
-        B = []
-
-        B.append(A[0])
-        B.append(A[1] + r * B[0])
-
-        for i in range(2, len(A)):
-            B.append(A[i] + r * B[i - 1] + s * B[i - 2])
-
-        print(B)
-        return(B)
+	disc = r**2 + 4*s
+	if disc > 0:
+		r1 = (r + math.sqrt(disc))/2
+		r2 = (r - math.sqrt(disc))/2
+		i1 = 0
+		i2 = 0
+	else:
+		r1 = r/2
+		r2 = r1
+		i1 = math.sqrt(abs(disc))/2
+		i2 = -i1
 
 
-def calculate_dels(c_m, b_v):
-    delta = c_m[0][0] * c_m[1][1] - c_m[1][0] * c_m[0][1]
-    d1 = c_m[1][1] * b_v[0] - c_m[0][1] * b_v[1]
-    d2 = c_m[0][0] * b_v[1] - c_m[1][0] * b_v[0]
+def Baristow(a,nn,es,rr,ss,maxit):
+	r = rr
+	s = ss
+	n = nn
+	ier, esl, ea2 = 0,1,1
+	iter = 0
+	while !(n<3 or iter >= maxit):
+		iter = 0
+		while True:
+			iter = iter + 1
+			b[n] = a[n]
+			b[n-1] = a[n-1] + r*b[n]
+			c[n] = b[n]
+			c[n-1] = b[n-1] + r*c[n]
+			for i in [n-2,0,1]:
+				b[i] = a[i] + r*b[i+1] + s*b[i+2]
+				c[i] = b[i] + r*c[i+1] + s*c[i+2]
+			det = c[2]*c[2] - c[3]*c[1]
+			if det != 0:
+				dr = (-b[1]*c[2] + b[0]*c[3])/det
+				ds = (-b[0]*c[2] + b[1]*c[1])/det
+				r = r+dr
+				s = s+ds
+				if r != 0:
+					ea1 = abs(dr/r)*100
+					ea2 = abs(ds/s)*100
+				else:
+					r = r+1
+					s = s+1
+					iter = 0
+			if (ea1 <= es and ea2 <= es or iter >= maxit):
+				break
 
-##    print(c_m)
-    return(round(d1 / delta, precision), round(d2 / delta, precision))
+		r1,i1,r2,i2 = quadroot(r,s)
+		re[n] = r1
+		im[n] = i1
+		re[n-1] = r2
+		im[n-1] = i2
+		n = n-2
+		for i in [0,n]:
+			a[i] = b[i+2]
+		if iter < maxit:
+			if n==2:
+				r = -a[1]/a[2]
+				s = -a[0]/a[2]
+				r1,i1,r2,i2 = quadroot(r,s)
+				re[n] = r1
+				im[n] = i1
+				re[n-1] = r2
+				im[n-1] = i2
+			else:
+				re[n] = -a[0]/a[1]
+				im[n] = 0
+		else:
+			ier = 1
 
-    
-B = calculate_b(r, s)
-C = calculate_c(r, s, B)
-
-c_matrix = [[C[len(C) - 2], C[len(C) - 3]], [C[len(C) - 1], C[len(C) - 2]]]
-b_vector = [-B[len(B) - 2], -B[len(B) - 1]]
-
-del_r, del_s = calculate_dels(c_matrix, b_vector)
-r += del_r
-s += del_s
-cnt = 0
-
-while del_r != 0 and del_s != 0 and cnt < stopper:
-    if math.inf in B:
-        break
-    
-    B = calculate_b(r, s)
-    C = calculate_c(r, s, B)
-
-    c_matrix = [[C[len(C) - 2], C[len(C) - 3]], [C[len(C) - 1], C[len(C) - 2]]]
-    b_vector = [-B[len(B) - 2], -B[len(B) - 1]]
-
-    del_r, del_s = calculate_dels(c_matrix, b_vector)
-    r += del_r
-    s += del_s
-
-##    print(del_r, del_s)
-    
-    cnt += 1
-
-
-print("\nr and s converges to ", r, s)

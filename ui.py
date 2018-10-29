b4dc269ac828c425176d801fb7bdb59d40763e6b
Guru Prasath <guru98@live.com>
2018-10-29 19:01:29 +0530

    Added p5 helloworld sketches for testing

---
+++ ui.py
@@ -0,0 +1,16 @@
+from p5 import *
+
+def setup():
+    size(400,400)
+    title('Hello')
+    background(255)
+    fill(255,0,0)
+    text("Hello, World!",(200,200))
+    no_loop()
+
+
+def draw():
+    pass
+
+if __name__ == '__main__':
+    run()
---
+++ vis_ui.py
@@ -0,0 +1,7 @@
+from vispy import app, gloo
+
+canvas = app.Canvas(title="Hello World")
+canvas.show()
+
+if __name__ == '__main__':
+    app.run()

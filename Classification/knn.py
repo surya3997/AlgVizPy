a02be444e9100aa172eaebc64d721f4d233b4c6e
surya3997 <surya3997@gmail.com>
2019-02-09 01:22:03 +0530

    knn done

--- Classification/knn.py
+++ Classification/knn.py
@@ -1,4 +1,14 @@
 import numpy as np
+import operator
+
+def clean(data):
+    cleaned = []
+    for row in data:
+        if '?' in row:
+            continue
+        else:
+            cleaned.append(row)
+    return cleaned
 
 def getCancerData():
     import os, sys
@@ -7,11 +17,46 @@ def getCancerData():
     correct_path = prefix_path + path
     with open(correct_path, 'r') as f:
         content_with_header = f.read().splitlines()
-    content = content_with_header[1:]
-    result_x = list(map(lambda x: x.split(',')[1:-1], content))
-    result_y = list(map(lambda x: x.split(',')[-1], content))
+    unclean_content = content_with_header[1:]
+    content = clean(unclean_content)
+    result_x = list(map(lambda x: list(map(int, x.split(',')[1:-1])), content))
+    result_y = list(map(lambda x: int(x.split(',')[-1]), content))
     return(result_x, result_y)
 
+def leaveOneOut(x_data, y_data):
+    training_x = x_data[:-1]
+    training_y = y_data[:-1]
+    testing_x = x_data[-1]
+    testing_y = y_data[-1]
+    return training_x, training_y, testing_x, testing_y
+
+def eucleidian_distance(point1, point2):
+    if len(point1) == len(point2):
+        dist = 0
+        for i, j in zip(point1, point2):
+            dist += (i - j) ** 2
+        return np.sqrt(dist)
+
+def KNN(train_x, train_y, test_x, test_y, k, distance_func):
+    dist_dict = {}
+    for x in range(len(train_x)):
+        distance = distance_func(train_x[x], test_x)
+        dist_dict[x] = distance
+    sorted_dist = sorted(dist_dict.items(), key=operator.itemgetter(1))
+
+    freq_count = {}
+    for i in range(k):
+        result = train_y[sorted_dist[i][0]]
+        if result in freq_count:
+            freq_count[result] += 1
+        else:
+            freq_count[result] = 1
+
+    sorted_count = sorted(freq_count.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_count[0][0]
 
 data_x, data_y = getCancerData()
-print()
+train_x, train_y, test_x, test_y = leaveOneOut(data_x, data_y)
+k = 100
+predicted_class = KNN(train_x, train_y, test_x, test_y, k, eucleidian_distance)
+print(predicted_class)
---
+++ Statistics/bernoulli_test_mean.py
@@ -0,0 +1,29 @@
+import bernoulli_rand_var
+import matplotlib.pyplot as plt
+import numpy as np
+
+sample_cnts = 60
+epsilon = 0.1
+head_cnt = 0
+prob_list = list()
+
+for sample_cnt in range(1, sample_cnts):
+    for i in range(sample_cnt):
+        head = bernoulli_rand_var.bernoulli_trail()
+        head_cnt += head
+
+    prob = head_cnt / sample_cnt
+    # print(prob)
+    
+    greater_eps_cnt = 0
+    if abs(prob - 0.5) > epsilon:
+        greater_eps_cnt += 1
+
+    epsilon_prob = greater_eps_cnt/sample_cnt
+    print(epsilon_prob)
+    prob_list.append(epsilon_prob)
+
+prob_arr = np.array(prob_list)
+plt.hist(prob_arr, bins=20)
+plt.ylabel('No of times')
+plt.show()
